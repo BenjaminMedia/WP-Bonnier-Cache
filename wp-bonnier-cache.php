fafa0e38fd0b198414be532365aff3b035dc8c05
Magnus Flor Johansen <magnus@m-flor.dk>
2019-03-07 13:58:38 +0100

    WILL-1070 - Integrating new Redirect plugin

--- wp-bonnier-cache.php
+++ wp-bonnier-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Bonnier Cache
  * Plugin URI: http://bonnierpublications.com
  * Description: Bonnier Cache Plugin
- * Version: 3.0.0
+ * Version: 4.0.0
  * Author: Magnus Flor
  * Author URI: http://bonnierpublications.com
  */
